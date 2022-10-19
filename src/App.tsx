77d94703b28319cd350b612aaee318a2e7306064
suhinadutt <52886765+suhinadutt@users.noreply.github.com>
2022-10-19 17:16:47 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean /*intializing a show graph property/*
+
 }
 
 /**
@@ -22,6 +24,8 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false, /* intial state of graph is hidden*/
+
     };
   }
 
@@ -29,20 +33,29 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    if (this.state.showGraph) { /*to ensure that the graph does not render until the user cliicks the streaming button*/
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
-  }
-
+        let x = 0;
+        const interval = setInterval(() => {
+          DataStreamer.getData((serverResponds: ServerRespond[]) => {
+            this.setState({
+             data: serverResponds,
+           showGraph: true,
+            });
+          });
+          x++;
+          if (x > 1000) {
+            clearInterval(interval);
+          }
+        }, 100);
+       }
   /**
    * Render the App react component
    */
